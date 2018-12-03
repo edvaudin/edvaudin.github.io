eb07b9f2c891972c2f30b9369a9ad4e474e71fdb
edvaudin <edward@vaudin.net>
2018-12-03 11:52:31 +0000

    added link

--- js/random.js
+++ js/random.js
@@ -2,5 +2,13 @@ setTimeout(function(){
     var elem = document.getElementById("animation");
     elem.style.marginTop = "40vh";
 }, 1000);
-
-
+var min = document.getElementsByClassName("min")[0];
+var max = documnet.getElementsByClassName("max")[0];
+function getRndInteger(min, max) {
+    return Math.floor(Math.random()*(max - min + 1) + min);
+}
+function result(){
+    var displayResult = document.getElementsByClassName("result")[0];
+    var result = getRndInteger(min, max);
+    displayResult.textContent = result;
+}
