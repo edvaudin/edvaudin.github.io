46aee35466d935f6aa7786b3380167167fb9c16d
edvaudin <edward@vaudin.net>
2018-12-04 20:36:00 +0000

    isNaN

--- js/random.js
+++ js/random.js
@@ -6,7 +6,13 @@ setTimeout(function(){
 function getRndInteger(min, max) {
     var min = parseInt(document.getElementById("min").value);
     var max = parseInt(document.getElementById("max").value);
-    var result = Math.floor(Math.random()*((max - min) + 1) + min);
+    console.log(min);
     var displayResult = document.getElementsByClassName("result")[0];
-    displayResult.innerHTML = result;
+    if (isNaN(min) || isNaN(max)){
+        displayResult.innerHTML = "Enter a number in both fields."
+    } else {
+        var result = Math.floor(Math.random()*((max - min) + 1) + min);
+        displayResult.innerHTML = result;
+    }
+
 }
