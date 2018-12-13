31b0bac13034d68f140497d27cf3eb6f02cbbf2d
edAsus <edward@vaudin.net>
2018-12-13 13:29:56 +0000

    link3index animation

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ setTimeout(function(){
     document.getElementById("link1").style.marginLeft = "0";
     setTimeout(function(){
         document.getElementById("link2").style.marginLeft = "0";
+        setTimeout(function(){
+            document.getElementById("link3").style.marginLeft = "0";
+        },50);
     },100);
 },300);
 
