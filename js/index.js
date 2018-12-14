29a5209b45db34d7fc520996baf46a4834cba430
edAsus <edward@vaudin.net>
2018-12-14 15:53:20 +0000

    fixed snow

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 
 
-setTimeout(function(){
-    document.getElementById("link1").style.marginLeft = "0";
-    setTimeout(function(){
-        document.getElementById("link2").style.marginLeft = "0";
-        setTimeout(function(){
-            document.getElementById("link3").style.marginLeft = "0";
-        },50);
-    },100);
-},300);
 
-function init(){
+
+function body_onload(){
     var he = document.getElementById("anim");
     he.classList.remove("anim");
+    setTimeout(function(){
+        document.getElementById("link1").style.marginLeft = "0";
+        setTimeout(function(){
+            document.getElementById("link2").style.marginLeft = "0";
+            setTimeout(function(){
+                document.getElementById("link3").style.marginLeft = "0";
+            },50);
+        },100);
+    },300);
+    
+    snowStorm.init();
 }
+
