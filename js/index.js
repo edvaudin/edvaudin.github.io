420e07e0fb7c3e7a69d25cdc574f99fb930585dc
Edward Vaudin <edward@vaudin.net>
2023-02-11 21:47:47 +0000

    Creating three tiles landing page template

--- js/index.js
+++ js/index.js
@@ -1,13 +1,2 @@
-function body_onload(){
-    setTimeout(function(){
-        document.getElementById("link1").style.marginLeft = "0";
-        setTimeout(function(){
-            document.getElementById("link2").style.marginLeft = "0";
-            setTimeout(function(){
-                document.getElementById("link3").style.marginLeft = "0";
-            },50);
-        },100);
-    },300);
 
-}
 
