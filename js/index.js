66a3dda6dbf483bf231c76003f2f4b08afdfc43f
Edward Vaudin <edward@vaudin.net>
2023-04-01 22:47:44 +0100

    Add links and change styling

--- js/index.js
+++ js/index.js
@@ -12,27 +12,40 @@ var softwareImages = [
     "img/software-images/focusanger.jpg",
     "img/software-images/taskbot.png"
 ];
-var slideRate = 3000;
+var slideRate = 2000;
 
 function cycleBackgroundImages(images, targetElement) {
     let index = 0;
   
     function updateBackgroundImage() {
-      const imageUrl = images[index];
-      targetElement.style.backgroundImage = "url(" + imageUrl + ")";
       index = (index + 1) % images.length;
+      const imageUrl = images[index];
+      targetElement.style.backgroundImage = `url(${imageUrl})`;
     }
   
-    updateBackgroundImage(); // Set initial background image
+    updateBackgroundImage();
   
-    setInterval(updateBackgroundImage, slideRate);
+    setInterval(updateBackgroundImage, (Math.random() + 1) * slideRate);
+}
+
+function fadeToNextImage(target){
+    return new Promise(function (resolve, reject) {
+        var delta = 0.01;
+        var id = setInterval(changeOpacity, 10);
+        function changeOpacity(){
+            target.style.opacity -= delta;
+            if (target.style.opacity <= 0){
+                clearInterval(id);
+                resolve();
+            }
+        }
+    })
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     const musicPanel = document.querySelector(".music-panel");
-const gamesPanel = document.querySelector(".games-panel");
-const softwarePanel = document.querySelector(".software-panel");
-console.log(musicPanel);
+    const gamesPanel = document.querySelector(".games-panel");
+    const softwarePanel = document.querySelector(".software-panel");
     cycleBackgroundImages(musicImages, musicPanel);
     cycleBackgroundImages(gamesImages, gamesPanel);
     cycleBackgroundImages(softwareImages, softwarePanel);
